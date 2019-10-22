d68263ae6d64bd1e9396ddc76b11acc018c91632
jonathanrush <51374448+jonathanmrush@users.noreply.github.com>
2019-10-22 13:11:20 -0400

    added liquid transition

---
+++ Liquid Image Transition/app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "./img/no.jpeg",
+  image2: "./img/go.jpeg",
+  angle: Math.PI / 8,
+  dissplacementImage: "./diss.png"
+});
