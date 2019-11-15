2dbc039d4e4b5f53981c52b6057ee93f80e32604
jonathanrush <51374448+jonathanmrush@users.noreply.github.com>
2019-11-15 13:54:23 -0500

    Added Project to-do list (WIP)

---
+++ To-do List JS App/JS/app.js
@@ -0,0 +1,6 @@
+//User Clicks on the add button adds text to to-do list
+document.getElementById("add").addEventListener("click", function() {
+  var value = document.getElementById("item").value;
+  if (value) {
+  }
+});
